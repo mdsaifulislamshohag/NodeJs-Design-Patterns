9e8bd4106d03e69a5704135e578d584133e066a9
saifuldev0 <mdsaifulislamshohag79@gmail.com>
2022-11-10 01:01:52 +0600

    counter.js : exports

---
+++ The-Module-System/counter.js
@@ -0,0 +1,17 @@
+// let count = 0;
+
+
+// function increment (){
+//     count++
+//     console.log(count)
+// }
+
+// increment();
+
+
+export let count = 0;
+
+export function increment (){
+    count++
+    console.log(count)
+}
--- The-Module-System/server.js
+++ The-Module-System/server.js
@@ -4,7 +4,7 @@ const Logger = require('./Logger');
 const Logger2 = require('./Logger2')
 //const LoggerModule = require('./Logger3')
 //import {log} from './Logger3.js'
-
+//const {count, increament} = require('./counter');
 
 app.get('/logger', (req, res) => {
     const dbLogger = new Logger("DB");
